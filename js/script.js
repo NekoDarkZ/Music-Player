5f302fe039c8bc5848c84f847c23052dba140faf
Sebastián Murquio Castillo <sebastian.murquio@alumnos.ucn.cl>
2023-10-18 16:59:08 -0300

    feat(player): play/pause music and change

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-// Get HTML elements
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Get HTML elements
 const audio = document.querySelector('audio');
 const musicCover = document.querySelector('.music-cover img');
 const musicTitle = document.querySelector('.music-info h2');
 const musicArtist = document.querySelector('.music-info h3');
-const progressBar = document.querySelector('.progress-bar');
+const progressBar = document.querySelector('.progress');
 const playButton = document.querySelector('.play');
 const prevButton = document.querySelector('.prev');
 const nextButton = document.querySelector('.next');
+const timeElapsed = document.getElementById('timeElapsed');
+const durationDisplay = document.getElementById('duration');
 
 // Define song data
 const songs = [
@@ -67,16 +71,29 @@ function updateSong() {
 }
 
 function updatePlayButton() {
-    playButton.innerHTML = audio.paused ? '<img src="images/stop_and_play_fill_reverse.svg" alt="play">' : '<img src="images/pause_fill.svg" alt="pause">';
+    playButton.innerHTML = audio.paused ? '<img src="images/play_fill.svg" alt="play">' : '<img src="images/pause.svg" alt="pause">';
 }
 
-// Update progress bar as audio plays
+// Update progress bar and time displays as audio plays
 audio.addEventListener('timeupdate', () => {
-    const progress = audio.currentTime / audio.duration;
-    progressBar.style.width = `${progress * 100}%`;
+    const progress = (audio.currentTime / audio.duration) * 100;
+    progressBar.style.width = `${progress}%`;
+
+    // Actualiza el tiempo transcurrido
+    const elapsedMinutes = Math.floor(audio.currentTime / 60);
+    const elapsedSeconds = Math.floor(audio.currentTime % 60);
+    timeElapsed.textContent = `${elapsedMinutes}:${elapsedSeconds}`;
+
+    // Actualiza la duración total
+    const totalMinutes = Math.floor(audio.duration / 60);
+    const totalSeconds = Math.floor(audio.duration % 60);
+    durationDisplay.textContent = `${totalMinutes}:${totalSeconds}`;
 });
 
-// Reset progress bar when song ends
+// Reset progress bar and time displays when song ends
 audio.addEventListener('ended', () => {
     progressBar.style.width = '0%';
+    timeElapsed.textContent = '0:00';
 });
+});
+
